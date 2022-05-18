837f06a396d8b117907519739fa6a6d659780c00
RenanMoreira92 <renan.s.7@hotmail.com>
2022-05-18 11:14:53 -0300

    Criando classe Curso

---
+++ src/br/com/alura/TestaCurso.java
@@ -0,0 +1,24 @@
+package br.com.alura;
+
+import java.util.List;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+		List<Aula> aulas = javaColecoes.getAulas();
+
+		//javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando coleçoes", 22));
+		
+		
+
+		System.out.println(javaColecoes.getAulas());
+
+	}
+
+}
